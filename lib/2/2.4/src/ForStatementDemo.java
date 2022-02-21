36261873d40ec0cd69741ddf0f5fbca6c1d35e41
HEX <thehexreader@gmail.com>
2022-02-21 23:56:53 +0300

    added from 2.1 to 2.6

---
+++ lib/2/2.4/src/ForStatementDemo.java
@@ -0,0 +1,10 @@
+public class ForStatementDemo {
+    public static void main(String[] args){
+        int count;
+
+        for (count = 0; count < 5; count++){
+            System.out.println("Переменная цикла равна " + count);
+        }
+        System.out.println("Цикл окончен");
+    }
+}
